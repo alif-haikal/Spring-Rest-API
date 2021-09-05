a3e8bc6226049e0bfcb97c8ef8e1ce3e0e67b22a
alif haikal <alifhaikal.1992@gmail.com>
2021-09-06 06:29:19 +0800

    basic Authentication

--- src/main/java/com/pokka/rest/webservice/restfulwebservice/basic/auth/BasicAuthenticationController.java
+++ src/main/java/com/pokka/rest/webservice/restfulwebservice/basic/auth/BasicAuthenticationController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 public class BasicAuthenticationController {
 	
